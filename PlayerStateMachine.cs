acc2318a4f9b64acf88e8ba3983cca2f5ef0c25a
samuelLegus <samuel.legus@live.com>
2015-03-27 00:16:06 -0700

    ...

---
+++ PlayerStateMachine.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using Sam;
+
+namespace Sam
+{
+	public class PlayerStateMachine : StateMachine 
+	{
+		//Enum is not supported by base class
+		private enum PlayerStates : int
+		{
+			IDLE = 0,
+			MOVE = 1,
+			ATTACK = 2,
+			STUNNED = 3
+		};
+
+		//Enum support implemented here, could just integers but enums are nice for clarity.
+		void SetCurrentState(PlayerStates ID)
+		{
+			CurrentState = states[(int)ID];
+		}
+
+		void AddState(State state)
+		{
+			states.Add (state.ID, state);
+		}
+			        
+		void Start()
+		{
+			IdleState idleState = new IdleState((int)PlayerStates.IDLE);
+			AttackState attackState = new AttackState((int)PlayerStates.ATTACK);
+
+			AddState (idleState);
+			AddState (attackState);
+
+			stateTransitions.Add( new StateTransition((int)PlayerStates.IDLE, 
+			                                          (int)PlayerStates.ATTACK,
+			                                          () => Input.GetMouseButtonDown (0)));
+
+
+			stateTransitions.Add( new StateTransition((int)PlayerStates.ATTACK, 
+			                                          (int)PlayerStates.IDLE,
+			                                          () => Input.GetMouseButtonDown (1)));
+
+			CurrentState = states[(int)PlayerStates.IDLE];
+			CurrentMethod = CurrentState.Update;
+
+		}
+	}
+	
+	public class IdleState : State
+	{
+		public IdleState(int id) : base(id)
+		{
+
+		}
+	
+		public override void Start()
+		{
+			Debug.Log ("IDLE START");
+		}
+
+		public override void End()
+		{
+			Debug.Log ("IDLE END");
+		}
+
+		public override void Update()
+		{
+		
+		}
+	}
+
+	public class AttackState : State
+	{
+		public AttackState(int id) : base (id)
+		{
+			
+		}
+		
+		public override void Start()
+		{
+			Debug.Log ("ATTACK START");
+		}
+		
+		public override void End()
+		{
+			Debug.Log ("ATTACK END");
+		}
+		
+		public override void Update()
+		{
+			
+		}
+	}
+}
+
---
+++ StateMachine.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Sam
+{
+	public abstract class State 
+	{
+		public abstract void Start();
+		public abstract void End();
+		public abstract void Update();
+
+		public State(int id)
+		{
+			this.ID = id;
+		}
+		public int ID;				// Make this an enum in the child class if possible.
+	}
+	
+	public class StateTransition
+	{
+		public int From {get; private set;}
+		public int To {get; private set;}
+		public Func<bool> Condition {get; private set;}
+
+		public StateTransition(int from, int to, Func<bool> condition)
+		{
+			From = from;
+			To = to;
+			Condition = condition;
+		}
+	}
+	
+	public abstract class StateMachine : MonoBehaviour 
+	{
+		protected delegate void Method();
+		protected Method CurrentMethod = new Method( () => {});
+		protected State CurrentState;
+		protected List<StateTransition> stateTransitions = new List<StateTransition>();
+		protected Dictionary< int, State> states = new Dictionary<int, State>();
+
+		//If you override update don't forget to call ManageTransitions() and CurrentMethod()
+		protected virtual void Update()
+		{
+			CurrentMethod();
+			ManageTransitions ();
+		
+			//Debug.Log (CurrentState.ID);
+		}
+		
+		protected void ManageTransitions()
+		{
+			foreach(StateTransition transition in stateTransitions)
+			{
+				if(CurrentState.ID == transition.From && transition.Condition() == true)
+				{
+					CurrentState.End();
+					CurrentState = states[transition.To];
+					CurrentState.Start();
+					CurrentMethod = CurrentState.Update;
+				}
+			}
+		}
+	}
+}
+
